c837dcd856b74a5427f67d861c683606da08372d
Brad Rucker <bruck63@gmail.com>
2023-03-06 19:11:45 -0600

    Finished displaying the search results form. Passes all tests in TestTaskFour

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -37,7 +37,7 @@ public class SearchController {
         }  else {
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
         }
-        model.addAttribute("title", "Jobs with" + searchType + ": " + searchTerm);
+        model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
 
         model.addAttribute("jobs", jobs);
         model.addAttribute("columns", columnChoices);
